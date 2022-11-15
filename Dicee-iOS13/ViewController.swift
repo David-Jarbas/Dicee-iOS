4aff75856f370b1cea6882fdeb56ac4e6b47da29
David-Jarbas <david.jarbas97@gmail.com>
2022-11-15 12:05:21 -0300

    first-commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // IBOutlet faz referencia ao UIkit e linka a imagem com o código.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+   
+    // o codigo sera acionado quando pressionar o botao (action)
+    @IBAction func PressButton(_ sender: UIButton) {
+        
+        let diceImages = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ]
+        // Esta linha de codigo apresenta as imagens de forma aleatória
+        diceImageViewOne.image = diceImages[Int.random(in:0...5)]
+        diceImageViewTwo.image = diceImages[Int.random(in:0...5)]
+        
+        
+        /*
+         Também podemos escrever desta maneira ->
+         diceimageViewOne.image = diceImagens.RandomElement()
+         
+         A linha acima checa quantos elementos existem no array e irá apresentar aleatoriamente
+         */
     }
-
-
+    
 }
-
